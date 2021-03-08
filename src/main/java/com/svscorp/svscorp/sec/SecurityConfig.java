ed6e9b65bc97c462dad5f0c38e52aad1e9f82856
altafmhss <36732944+altafmhss@users.noreply.github.com>
2021-03-08 11:27:18 +0530

    Add files via upload

---
+++ src/main/java/com/svscorp/svscorp/sec/SecurityConfig.java
@@ -0,0 +1,25 @@
+//package com.svscorp.svscorp.sec;
+//
+//
+//import com.azure.spring.aad.webapp.AADWebSecurityConfigurerAdapter;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure( HttpSecurity http ) throws Exception {
+//
+//        http.authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .oauth2Login()
+//                .userInfoEndpoint();
+//
+//    }
+//}
